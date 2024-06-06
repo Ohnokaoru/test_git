2bbef6933220de27f6bd9f0ab353c7cdad1cfe91
Kaoru <kaoru@gmail.com>
2024-06-06 09:09:58 +0800

    完成game.py邏輯內容

--- game.py
+++ game.py
@@ -1,5 +1,13 @@
 import random
 
-for i in range(10):
-    x = random.randint(1, 50)
-    print(x)
+
+x = random.randint(1, 50)
+print(x)
+for i in range(5):
+    guess = input("猜數字:")
+
+    if guess == str(x):
+        print("答對")
+        break
+    else:
+        print(f"猜錯了，你猜{i+1}次")
